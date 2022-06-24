c5ad07ebf3d5cfa96df6cc4bde3b67e1efdf6693
Nermien <60138319+nermien990@users.noreply.github.com>
2022-06-24 22:44:43 +0200

    Time: 533 ms (95.23%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
 select name as 'Customers'
 from Customers c
-where not exists(select * from Orders o 
+where not exists(select name from Orders o 
                      where c.id = o.customerId )
